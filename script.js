5f3aaa7318037677273f3c3faa157664cce785ef
Jay <fatloser420@gmail.com>
2023-11-25 12:32:18 -0500

    did

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-$(document).on("load", () => {
-  fetch("http://shibe.online/api/shibes?count=[1]&urls=[true]&httpsUrls=[true]")
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch(console.error(console.log("sucker")));
-});
+let goob = () => {
+  fetch("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true")
+    .then((res) => res.json())
+    .then((data) => $("#guh").attr("src", data));
+};
